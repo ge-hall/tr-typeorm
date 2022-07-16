b5d9ff31ab3d4bec9a59ba744d99ab0a92cccbce
Andrew Ge-Hall <ge-hall@outlook.com>
2022-07-17 09:10:17 +1000

    feat(inc): added user proto and registered USER_SERVICE_NAME

--- api-gateway/src/user/user.module.ts
+++ api-gateway/src/user/user.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { USER_SERVICE_NAME, USER_PACKAGE_NAME } from './user.pb';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: USER_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50051',
+          package: USER_PACKAGE_NAME,
+          protoPath: 'node_modules/tr-typeorm-proto/proto/user.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
