aaa2869bb74984d94ed1c1489a7a8ce6814df38b
Andrew Ge-Hall <ge-hall@outlook.com>
2022-07-16 21:49:16 +1000

    trial:Gdiff

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory  } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
