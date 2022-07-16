82875228a4b7ce43e52f4dd41c4e5063d6b0db12
Andrew Ge-Hall <ge-hall@outlook.com>
2022-07-16 23:40:27 +1000

    feat:routing

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Header } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get()
+  @Header('Cache-Control', 'none')
+  getRoot(@Req() req: Request): string {
+    console.log(`Request ${req.ip}`);
+    return 'root path handler';
   }
 }
